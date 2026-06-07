a78b79aa43f5c9a9905d4b38bf3a518184f25b9d
Fernando Oliveira <ofernando58@gmail.com>
2026-06-07 18:46:43 -0300

    refactor: Update app_main to include FreeRTOS task handling and loop for periodic messages

--- main/main.c
+++ main/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
 void app_main(void)
 {
-    printf("Hello World!\n");
+    int i = 0;
+    while(1) {
+        printf("Hello World, ESP1... %d!\n", i);
+        // printf("Hello World, ESP2... %d!\n", i);
+        i++;
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
